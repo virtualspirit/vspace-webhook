25d1954933f044b6691c6d151b7a5cb7ac5d5b5c
Vederis Leunardus <Leunardus.vederis714@gmail.com>
2022-11-03 10:39:51 +0000

    Fix the Await error

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const github = require('@actions/github');
 
 // create post request to virtualspace api 
 const axios = require('axios');
+const qs = require('qs');
 
-try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const http_post_header = {
-        'Content-Type': 'application/json',
-        'secret-key': core.getInput('secret-key')
-    }
-    const http_post_body = {
-        'text': core.getInput('messages'),
-    }
-    const http_post_url = core.getInput('webhook-url')
-    const http_post_response = await axios.post(http_post_url, http_post_body, { headers: http_post_header })
-    // return status code 
-    console.log(`Status: ${http_post_response.status}`);
+async function run() {
+    try {
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const http_post_header = {
+            'Content-Type': 'application/json',
+            'secret-key': core.getInput('secret-key')
+        }
+        const http_post_body = {
+            'text': core.getInput('messages'),
+        }
+        const http_post_url = core.getInput('webhook-url')
+        const http_post_response = await axios.post(http_post_url, http_post_body, { headers: http_post_header })
+        
+        console.log(http_post_response.data)
 
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run()
